b010805c3085cf142133d0905f2b146dcdceef74
Alex Huarag <alexhuarag@gmail.com>
2022-06-03 22:53:30 -0500

    New 03

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -127,12 +127,37 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
 // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var prod;
+      if(arguments[i].length != 0)
+        {
+            for (var i=1;i<arguments[i].length;i++)
+            {
+                prod = arguments[i]*prod;
+
+            }
+              return prod;
+        }
+        else
+          return "0";
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+
+  for (var i =1;i<arreglo.length;i++)
+  {
+    if(arreglo[i]>18)
+    arreglo[i] = arreglo[i];
+
+  }
+
+  for (var i =1;i<arreglo.length;i++)
+  {
+    return arreglo[i];
+
+  }
 
 }
 
